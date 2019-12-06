fb77ccf6ac2371f9312f8765aa6a99545b2077b0
Grumodas <grumodas.kestutis@gmail.com>
2019-12-06 15:35:27 +0200

    Add project files.

---
+++ MazeSolver/Maze.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace MazeSolver
+{
+    public class Maze
+    {
+        int height { get; set; }
+        int width {get; set;}
+        public int[,] array;
+        public Point current { get; set; }
+        public Point start { get; set; }
+        public List<Point> visited = new List<Point>();
+        public Stack<Point> path = new Stack<Point>();
+
+        public Maze(int width, int height)
+        {
+            array = new int[width, height];
+            this.width = width;
+            this.height = height;
+        }
+
+        public void SetCurrent(Point value)
+        {
+            visited.Add(value);
+            path.Push(value);
+            this.current = value;
+            start = value;
+        }
+
+        public int SetNewCurrent(Point value)
+        {
+            if (array[value.X, value.Y] != 0)
+            {
+                Console.WriteLine("The position you entered is illegal. Try again!");
+                return -1;
+            }
+            array[current.X, current.Y] = 0;
+            visited.Remove(current);
+            path.Pop();
+
+            array[value.X, value.Y] = 2;
+            visited.Add(value);
+            path.Push(value);
+            this.current = value;
+            start = value;
+            return 0;
+        }
+
+        public void Print()
+        {
+            for (int r = 0; r < width; r++)
+            {
+                for (int c = 0; c < height; c++)
+                {
+                    Console.Write(array[r, c] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public int Move(Point dest)
+        {
+            if (dest.X < 0 | dest.Y < 0 | dest.X > width | dest.Y > height)
+            {
+                return -1;
+            }
+            if (array[dest.X, dest.Y] != 1)
+            {
+                array[current.X, current.Y] = 0;
+                array[dest.X, dest.Y] = 2;
+                //Console.WriteLine("was " + current.X + ", " + current.Y + " NEW IS " + dest.X + "," + dest.Y);
+                current = dest;
+                path.Push(current);
+                visited.Add(current);
+                Print();
+                return 0;
+            } else
+            {
+                return 1;
+            }
+        }
+
+        public void Solve()
+        {
+            Point dest;
+
+            if (foundExit())
+            {
+                return;
+            }
+
+            //try going left
+            if (!visited.Contains(dest = new Point(current.X, current.Y - 1)) && Move(dest) == 0)
+            {
+                Solve();
+            }
+            
+            if (foundExit())
+            {
+                return;
+            }
+            
+            //try going right
+            if (!visited.Contains(dest = new Point(current.X, current.Y + 1)) && Move(dest) == 0)
+            {
+                Solve();
+            }
+
+            if (foundExit())
+            {
+                return;
+            }
+
+            //try going down
+            if (!visited.Contains(dest = new Point(current.X + 1, current.Y)) && Move(dest) == 0)
+            {
+                Solve();
+            }
+
+            if (foundExit())
+            {
+                return;
+            }
+
+            //try going up
+            if (!visited.Contains(dest = new Point(current.X - 1, current.Y)) && Move(dest) == 0)
+            {
+                Solve();
+            } 
+
+            if (foundExit())
+            {
+                return;
+            }
+            //Point backtrack = path.Pop();
+            MoveBack();
+            return;
+            }
+
+        //backtrack
+        public void MoveBack()
+        {   
+            Point back = path.Pop();
+            if (back.X == current.X && back.Y == current.Y)
+            {
+                back = path.Pop();
+            }
+
+            array[current.X, current.Y] = 0;
+            array[back.X, back.Y] = 2;
+            //Console.WriteLine("BACKTRACK! Was " + current.X + ", " + current.Y + " NEW IS " + back.X + "," + back.Y);
+            current = back;
+            Print();
+            path.Push(current);
+        }
+
+
+
+        public bool foundExit()
+        {
+            if (current.X == 0 | current.X == width - 1 |
+                current.Y == 0 | current.Y == height - 1)
+            {
+
+                Point temp;
+                temp = path.Pop();
+                //Console.Write("(" + temp.X + "," + temp.Y + ")  ");
+                if (path.Count != 0)
+                {
+                    array[temp.X, temp.Y] = 3;
+                } else
+                {
+                    array[temp.X, temp.Y] = 2;
+                }
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ MazeSolver/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace MazeSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            for (int i = 0; i < 3; i++)
+            {
+                directory = Directory.GetParent(directory).FullName;
+            }
+            string sourceFile = directory + @"\RPAMaze.txt";
+
+            StreamReader sr = File.OpenText(sourceFile);
+
+            //reading height and width
+            string singleLine = sr.ReadLine();
+            string[] heightAndWidth = singleLine.Split(' ');
+            int height = Int32.Parse(heightAndWidth[0]);
+            int width = Int32.Parse(heightAndWidth[1]);
+            Maze maze = new Maze(width, height);
+
+            //parsing file line by line
+            for (int row = 0; (singleLine = sr.ReadLine()) != null; row++)
+            {
+                int col = 0;
+                foreach (char c in singleLine) {
+
+                    if (c != ' ')
+                    {
+                        maze.array[row, col++] = c - '0';
+                        
+                        if (c == '2')
+                        {
+                            maze.SetCurrent(new Point(row, col - 1));
+                            //Console.WriteLine("Start position is (" + row + "," + (col - 1) + ")");
+                        }
+                    }
+                }
+            }
+
+            Console.Write("Hello. Would you like to select a custom starting position? Type \"yes\" or \"no\": ");
+            string customStartAnswer = Console.ReadLine();
+            if (customStartAnswer.ToUpper() == "YES")
+            {
+                Console.WriteLine("Please select a starting position (and remember - index starts at 0): ");
+                int newStartCol, newStartRow;
+                do
+                {
+                    Console.Write("Row: ");
+                    Int32.TryParse(Console.ReadLine(), out newStartRow);
+                    Console.Write("Column: ");
+                    Int32.TryParse(Console.ReadLine(), out newStartCol);
+                } while (maze.SetNewCurrent(new Point(newStartRow, newStartCol)) != 0);
+            }
+
+            Console.WriteLine("Your starting position is (" + maze.current.X + "," + maze.current.Y + ")");
+
+            maze.Print();
+            maze.Solve();
+            Console.WriteLine("The path was saved to Result.txt. Thanks for playing :)");
+
+            string result = directory + @"\Result.txt";
+
+            //creating Result.txt file
+            string mazePrint = "";
+            for (int r = 0; r < width; r++)
+            {
+                for (int c = 0; c < height; c++)
+                {
+                    if (maze.array[r, c] == 3)
+                    {
+                        mazePrint += "* ";
+                    } else
+                    {
+                        mazePrint += maze.array[r, c] + " ";
+                    }
+                }
+
+                mazePrint += Environment.NewLine;
+            }
+
+            File.WriteAllText(result, "The path is marked with *" + Environment.NewLine);
+            File.AppendAllText(result, mazePrint);
+        }
+
+    }
+}
